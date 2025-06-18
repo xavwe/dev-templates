d8f542d9c32be198dab5d7dcc788ed37f5d600b0
nu <xaver.wenhart@proton.me>
2025-06-18 22:22:35 +0200

    feat: create top level flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Multi-language Nix flake templates";
+
+  outputs = { self }: {
+    templates = {
+      bash = {
+        path = ./bash;
+        description = "Lua development environment";
+      };
+      cpp = {
+        path = ./cpp;
+        description = "C++ development environment";
+      };
+      python = {
+        path = ./python;
+        description = "Python development environment";
+      };
+      groff = {
+        path = ./groff;
+        description = "Groff development environment";
+      };
+    };
+  };
+}
+
